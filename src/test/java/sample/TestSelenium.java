c87892fab96b9d86407b39256b35186b7d95b16e
eleve <samonil@hotmail.fr>
2018-06-22 14:17:57 +0200

    TEST(*) Ajout test selenium

--- src/test/java/sample/TestSelenium.java
+++ src/test/java/sample/TestSelenium.java
@@ -1,5 +1,8 @@
 package sample;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -8,12 +11,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestSelenium {
+
+    /**
+     * Permet de set le path du driver avant les tests
+     */
     @Before
     public void setUp(){
         System.setProperty("webdriver.gecko.driver", "/home/eleve/Documents/webapp-sample/geckodriver");
     }
 
 
+    /**
+     * Effectue un test de recherche google
+     */
     @Test
     public void testGoogleSearch() throws Exception {
         WebDriver driver = new FirefoxDriver();
@@ -21,6 +31,26 @@ public class TestSelenium {
         Thread.sleep(5000);
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("Adi3000");
+        searchBox.submit();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+    @Test
+    public void testSeleniumSearch() throws Exception{
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.seleniumhq.org/");
+        Thread.sleep(5000);
+        WebElement searchBox = driver.findElement(By.linkText("Projects"));
+        searchBox.click();
+        Thread.sleep(5000);
+        WebElement searchLink = driver.findElement(By.linkText("Selenium WebDriver"));
+        searchLink.click();
+        WebElement searchLink2 = driver.findElement(By.linkText("FirefoxDriver"));
+        searchLink2.click();
+
+        List<WebElement> searchFirefoxText = driver.findElements(By.linkText("org.openqa.selenium.firefox"));
+        Assert.assertEquals(1, searchFirefoxText.size());
         searchBox.submit();
         Thread.sleep(5000);
         driver.quit();
